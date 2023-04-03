f6ef86a08c5323f3b2c961008c75addf456907c8
franco.leyes <franco.leyes@hexium.com.ar>
2023-04-03 03:16:41 -0300

    Agregado de dependencia

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-// rollup.config.js
-export default {
-    input: 'src/main.js',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'cjs'
-    },
-    external: ['firebase/app'] // Agrega 'firebase/app' a la opción 'external'
-  };
